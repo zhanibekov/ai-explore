056d4790fc1e90145c8b3441141c92fce01ca613
Yelaman <elamanzhanibekov@gmail.com>
2026-01-16 12:26:19 +0500

    added hooks useStreamPlayer

---
+++ src/app/hooks/useStreamPlayer.ts
@@ -0,0 +1,68 @@
+import { useRef, useState } from "react";
+import { StreamEvent } from "../types/stream";
+import { STATUSES } from "../constants/statuses";
+
+export const useStreamPlayer = (
+  events: StreamEvent[],
+  onEvent: (event: StreamEvent) => void
+) => {
+  const [status, setStatus] = useState(STATUSES.IDLE);
+  const [index, setIndex] = useState(0);
+  const indexRef = useRef(0);
+  const timer = useRef(0);
+
+  const delay = () => {
+    return Math.floor(Math.random() * 100) + 50;
+  };
+
+  const play = () => {
+    if (events.length === 0) {
+      return;
+    }
+    if (status == STATUSES.STREAMING) {
+      return;
+    }
+    setIndex(0);
+    setStatus(STATUSES.STREAMING);
+    indexRef.current = 0;
+    timer.current = setTimeout(tick, delay()) as any;
+  };
+
+  const tick = () => {
+    const i = indexRef.current;
+    const ev = events[i];
+
+    if (!ev) {
+      stop();
+      setStatus(STATUSES.DONE);
+      return;
+    }
+
+    onEvent(ev);
+
+    if (ev.event === "done") {
+      stop();
+      setStatus(STATUSES.DONE);
+      return;
+    }
+
+    if (ev.event === "error") {
+      stop();
+      setStatus(STATUSES.ERROR);
+      return;
+    }
+
+    indexRef.current = i + 1;
+    timer.current = setTimeout(tick, delay()) as any;
+  };
+
+  const stop = () => {
+    clearTimeout(timer.current);
+    timer.current = 0;
+    indexRef.current = 0;
+    setIndex(0);
+    setStatus(STATUSES.IDLE);
+  };
+
+  return { status, play, stop };
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,26 @@
 "use client";
 import { useState } from "react";
 import { parseJsonl } from "./utils/parseJsonl";
-import { STATUSES } from "./constants/statuses";
 import { StreamEvent } from "./types/stream";
-
+import { useStreamPlayer } from "./hooks/useStreamPlayer";
 export default function Home() {
-  const [status, setStatus] = useState(STATUSES.IDLE);
   const [events, setEvents] = useState<StreamEvent[]>([]);
   const [streamedText, setStreamedText] = useState("");
+  const onEvent = (event: StreamEvent) => {
+    if (event.event === "token") {
+      setStreamedText((prev) => prev + event.data);
+    }
+  };
+  const { play, stop, status } = useStreamPlayer(events, onEvent);
+
   const handleFileChange = (event: any) => {
     const file = event.target.files[0];
     if (file) {
       const reader = new FileReader();
       reader.onload = (e) => {
-        // const content = e.target?.result;
-        // console.log(content);
         const parsedJsonl = parseJsonl(e.target?.result as string);
         setEvents(parsedJsonl);
         setStreamedText("");
-        setStatus(STATUSES.IDLE);
       };
       reader.readAsText(file);
     }
